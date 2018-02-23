8288c982d916bd026b1b0d4ed22e603d6c0f5348
DilipCoder <krdilip276@gmail.com>
2018-02-23 23:48:50 +0530

    listFilter updated

--- listFilter.py
+++ listFilter.py
@@ -1,10 +1,11 @@
 # data handling
 
-list_input = [int(x) for x in input("enter the integers for the list").split()]
+list_input = [int(x) for x in input("enter the integers for the list: ").split()]
 
+max_bound = int(input("enter maximum bound to shortlisting: "))
 # filtering the list 
 
-result = list(filter(lambda x: x<5, list_input))
+result = list(filter(lambda x: x< max_bound, list_input))
 
 # printing the result
 
